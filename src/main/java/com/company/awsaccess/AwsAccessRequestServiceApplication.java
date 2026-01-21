8bd57132372679c5a752377fa87df592b835e537
Manya Chandrashekar <manyachandrashekar@Manya.local>
2026-01-21 14:41:33 +0530

    bootstrap: spring boot project with gradle

---
+++ src/main/java/com/company/awsaccess/AwsAccessRequestServiceApplication.java
@@ -0,0 +1,13 @@
+package com.company.awsaccess;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsAccessRequestServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsAccessRequestServiceApplication.class, args);
+	}
+
+}
